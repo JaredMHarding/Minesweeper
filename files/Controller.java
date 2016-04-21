d88ebaa3cb00d80e7a9eb85e0119e43e23857e00
Jared Micheal Harding <jharding@sci255-1.labs.encs>
2016-04-21 16:02:45 -0700

    Just setting up a basic skeleton of the window and testing adding the buttons programatically.

--- files/Controller.java
+++ files/Controller.java
@@ -1,5 +1,35 @@
 package files;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
 
+    @FXML
+    private GridPane windowGP;
+    @FXML
+    private GridPane minefieldGP;
+    @FXML
+    private ChoiceBox difficultyCB;
+    @FXML
+    private Label nonminesLabel;
+
+    @FXML
+    private void initialize() {
+        windowGP.setGridLinesVisible(true);
+        minefieldGP.setGridLinesVisible(true);
+        difficultyCB.setItems(FXCollections.observableArrayList("Easy","Medium","Hard"));
+        nonminesLabel.setText("");
+        int mfHeight = minefieldGP.getRowConstraints().size();
+        int mfWidth = minefieldGP.getColumnConstraints().size();
+        for (int i = 0;i < mfWidth;i++) {
+            for (int j = 0; j < mfHeight; j++) {
+                minefieldGP.add(new Button(""), j, i);
+            }
+        }
+    }
 }
